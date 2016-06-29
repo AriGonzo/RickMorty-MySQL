55d7e9d96c7948290401a16338512938a1290a7f
Ariel Gonzalez <ArielGonzalez515@gmail.com>
2016-06-29 02:49:37 -0400

    went a bit overboard but had a good time doing it

---
+++ public/js/index.js
@@ -0,0 +1,109 @@
+coolnessPage = function(){
+	//clears the template container
+	clearTemplateDiv()
+
+	//API call with Ascending Order
+	multiCastCall('/coolness-chart/desc')
+};
+
+uncoolnessPage = function(){
+	//clears the template container
+	clearTemplateDiv()
+
+	//API call with Ascending Order
+	multiCastCall('/coolness-chart/asc')	
+};
+
+castPage = function(){
+	//clears the template container
+	clearTemplateDiv()
+
+	//API Call
+	multiCastCall('/cast');
+};
+
+attitudeChange = function(){
+	var attitude = $(this).val();
+	if (attitude == "false") {
+		clearTemplateDiv();
+		$('#templateContainer').html('<h2 class="text-center">Select an option above to get started!</h2>')
+		return false
+	}
+
+	singleCastCall('/attitude-chart/'+attitude)
+};
+
+multiCastCall = function(url){
+	//api call to get data
+	$.get(url, function(data){
+		
+		//for loop through the array of data received
+		for(var i = 0; i < data.length; i++) {
+
+			//creates the div to drop the template into and adds template 
+			var template = $('<div>');
+			template.addClass('template'+i);
+
+			//adds bootstrap class for grid
+			template.addClass('col-md-3')
+
+			//appends template div to templateContainer
+			$('#templateContainer').append(template);
+
+			//using templating library to drop data
+			$(".template"+i).loadTemplate("../templates/castTemplate.html",
+		    {
+		        image: data[i].image,
+		        name: data[i].name,
+		        id: data[i].id,
+		        coolness_points: "Coolness Points: " + data[i].coolness_points,
+		        attitude: "Attitude: " + data[i].attitude,
+		    });
+		}
+	});
+};
+
+singleCastCall = function(url){
+	$.get(url, function(castMember){
+		console.log(castMember);
+		$('#templateContainer').loadTemplate("../templates/singleTemplate.html", 
+			{
+	      image: castMember.image,
+	      id: "id: " + castMember.id,
+	      name: castMember.name,
+	      coolness_points: "Coolness Points: " + castMember.coolness_points,
+	      attitude: "Attitude: " + castMember.attitude,
+	      bio: castMember.description
+			});
+	});
+}
+
+getAttitudes = function(){
+	$.get('/attitudes', function(attitudeArray){
+		attitudeArray.forEach(function(attitudeObj){
+			var optionElement = $('<option>');
+			optionElement.text(attitudeObj.attitude);
+			$('#attitude').append(optionElement);
+		});
+	});
+};
+
+viewCharacter = function(){
+	var characterId = $(this).attr('id');
+	singleCastCall('/characters/'+characterId);
+}
+
+clearTemplateDiv = function(){
+	$('#attitude').val("false")
+	$('#templateContainer').empty();
+};
+
+//trigger on page load
+getAttitudes();
+
+//Button Triggers
+$('#castBtn').on('click', castPage);
+$('#coolnessBtn').on('click', coolnessPage);
+$('#uncoolnessBtn').on('click', uncoolnessPage);
+$(document).on('click', '.characterName', viewCharacter);
+$('#attitude').on('change', attitudeChange);
---
+++ server.js
@@ -0,0 +1,64 @@
+var express 	= require('express');
+var mysql 		= require('mysql');
+var app 			= express();
+var path			= require('path');
+var PORT			= 8080;
+
+var connection = mysql.createConnection({
+	host: 'localhost',
+	user: 'root',
+	password: process.argv[2],
+	database: 'favoriteShow_db'
+});
+
+connection.connect(function(err){
+	if (err) {
+		console.error('error connection:', err.stack);
+		return
+	}
+	console.log('connected!')
+});
+
+app.use(express.static('public'));
+
+app.get('/', function(){
+	res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.get('/cast', function(req, res){
+	connection.query('SELECT * FROM actors', function(err, result){
+		if (err) { throw err }
+		res.json(result);
+	});
+});
+
+app.get('/coolness-chart/:order', function(req, res){
+	connection.query('SELECT * FROM actors ORDER BY coolness_points ' + req.params.order.toUpperCase(), function(err, result){
+		if (err) { throw err }
+		res.json(result);
+	});
+});
+
+app.get('/attitude-chart/:att', function(req, res){
+	connection.query('SELECT * FROM actors WHERE ?', {attitude: req.params.att}, function(err, result){
+		if (err) { throw err }
+		res.json(result[0]);
+	});
+});
+
+app.get('/characters/:id', function(req, res){
+	connection.query('SELECT * FROM actors WHERE ?', {id: req.params.id}, function(err, result){
+		if (err) { throw err }
+		res.json(result[0]);
+	});
+});
+
+app.get('/attitudes', function(req, res){
+	connection.query('SELECT attitude FROM actors', function(err, results){
+		res.json(results)
+	});
+});
+
+app.listen(PORT, function(){
+console.log("listening on", PORT)
+});
